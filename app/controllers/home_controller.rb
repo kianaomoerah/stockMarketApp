cf440efd128a5f0bb31a7df463f31f6ec18fcb28
Kiana Omoerah <kiana.omoerah@gmail.com>
2023-07-28 16:47:54 -0400

    form response redirect error fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
 
   def index
     @api = StockQuote::Stock.new(api_key: $api_key)
+
+    if params[:stock]
+      @stock = StockQuote::Stock.quote(params[:stock])
+    end
   end
 
   def about 
